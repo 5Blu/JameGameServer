09f1a7023aaa121891e6419fddeb90d8c1efd453
james <james2004j.a@gmail.com>
2026-04-03 13:11:06 +0100

    svr

--- server.py
+++ server.py
@@ -10,8 +10,7 @@ async def handler(ws):
             print("Received:", message)
 
             # Echo back to sender
-            await ws.send(f"Server got: {message}")
-            await ws.send(f"Connected to Twokie VPS")
+            await ws.send(f"Server got: {message}\nConnected to Twokie VPS")
 
             # Or broadcast to everyone:
             # await asyncio.gather(*[c.send(message) for c in clients])
