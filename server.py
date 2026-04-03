877f071222ef8ba743ec3471b79bb6d795d4939a
james <james2004j.a@gmail.com>
2026-04-03 12:59:12 +0100

    updated message

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ async def handler(ws):
 
             # Echo back to sender
             await ws.send(f"Server got: {message}")
+            await ws.send(f"Connected to Twokie VPS")
 
             # Or broadcast to everyone:
             # await asyncio.gather(*[c.send(message) for c in clients])
