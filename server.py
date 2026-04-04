70e940026b4a5de325a724bb2680a13bbaf9b396
james <james2004j.a@gmail.com>
2026-04-04 12:03:09 +0100

    again

--- server.py
+++ server.py
@@ -117,9 +117,9 @@ async def handler(ws):
                 chc = "["
                 for i, c in enumerate(game.choices):
                     if c.target is not None:
-                        chc += f'"option":{i},"c_id":{c.c_id},"type":{c.type},"target":{c.target.name}'
+                        chc += f'"option":{i},"c_id":{c.card.cid},"type":{c.type},"target":{c.target.name}'
                     elif c.card is not None:
-                        chc + f'"option":{i},"c_id":{c.c_id},"type":{c.type},"target":"None"'
+                        chc + f'"option":{i},"c_id":{c.card.cid},"type":{c.type},"target":"None"'
                     else:
                         chc + f'"option":{i},"c_id":"None","type":{c.type},"target":"None"'
                 chc += "]"
